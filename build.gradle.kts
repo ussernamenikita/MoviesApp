71afa7de1909db8f8e33a22a06ac636f3831e1d0
nikita <nikitaatk@gmail.com>
2019-09-08 18:30:13 +0300

    Migrate to Gradle Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.3.50'
+    val kotlinVersion = "1.3.50"
     repositories {
         google()
         jcenter()
-        
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.6.0-alpha09'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:3.6.0-alpha09")
+        classpath(kotlin("gradle-plugin", kotlinVersion))
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -20,10 +19,9 @@ allprojects {
     repositories {
         google()
         jcenter()
-        
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+val clean by tasks.registering(Delete::class) {
+    delete(buildDir)
 }
---
+++ buildSrc/src/main/kotlin/config/Config.kt
@@ -0,0 +1,30 @@
+package config
+
+object Config {
+    const val targetSdkVersion = 28
+    const val minSdkVersion = 21
+    const val applicationID = "ru.test.moviesapp"
+    const val buildToolsVersion = "29.0.0"
+    const val compileSDKVersion = 28
+}
+
+object DepVersions {
+    const val coroutinesVersion = "1.3.1"
+    const val constraintVersion = "1.1.3"
+    const val ktxVersion = "1.1.0"
+    const val kotlinVersion = "1.3.50"
+    const val appCompatVersion = "1.1.0"
+    const val junitVersions = "4.12"
+}
+
+object Dependencies {
+    const val espressoCore = "androidx.test.espresso:espresso-core:3.2.0"
+    const val junitExt = "androidx.test.ext:junit:1.1.1"
+    const val junit = "junit:junit:${DepVersions.junitVersions}"
+    const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${DepVersions.kotlinVersion}"
+    const val appcompat = "androidx.appcompat:appcompat:${DepVersions.appCompatVersion}"
+    const val coreKtx = "androidx.core:core-ktx:${DepVersions.ktxVersion}"
+    const val constraintlayout = "androidx.constraintlayout:constraintlayout:${DepVersions.constraintVersion}"
+    const val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${DepVersions.coroutinesVersion}"
+    const val coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${DepVersions.coroutinesVersion}"
+}
